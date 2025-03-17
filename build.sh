9cb43a5be7e72e74b37b741cf84d56c4d2121422
Thomas Dunteman <104242297+LearningToPi@users.noreply.github.com>
2025-03-16 22:40:59 -0700

    update build script

--- build.sh
+++ build.sh
@@ -2,17 +2,28 @@
 
 
 VERSION=$1
-DISTRO='ubuntu'
-TAG='latest'
+SOURCE_DISTRO='ubuntu'
+SOURCE_TAG='latest'
+REGISTRY="docker.io"
+PACKAGE="learningtopi/vsftpd"
 
-docker pull docker.io/${DISTRO}:${TAG}
+docker pull docker.io/${SOURCE_DISTRO}:${SOURCE_TAG}
 RETCODE=$?
 if [ "${RETCODE}" -ne 0 ] ; then
-    echo "Failed to pull ${DISTRO}:{$TAG}.  Return code ${RECODE}"
+    echo "Failed to pull ${SOURCE_DISTRO}:{$SOURCE_TAG}.  Return code ${RECODE}"
     quit $RETCODE
 fi
 
-IMAGE_DISTRO=`docker image inspect ${DISTRO}:${TAG} -f '{{index .Config.Labels "org.opencontainers.image.ref.name"}}'`
-IMAGE_VERSION=`docker image inspect ${DISTRO}:${TAG} -f '{{index .Config.Labels "org.opencontainers.image.version"}}'`
+IMAGE_SOURCE_DISTRO=`docker image inspect ${SOURCE_DISTRO}:${SOURCE_TAG} -f '{{index .Config.Labels "org.opencontainers.image.ref.name"}}'`
+IMAGE_VERSION=`docker image inspect ${SOURCE_DISTRO}:${SOURCE_TAG} -f '{{index .Config.Labels "org.opencontainers.image.version"}}'`
 
-docker build -f Containerfile . -t learningtopi/vsftpd:${VERSION} -t learningtopi/vsftpd:latest --build-arg VERSION="${IMAGE_DISTRO}-${IMAGE_VERSION}"
+docker build -f Containerfile . -t $REGISTRY/$PACKAGE:${VERSION} -t $REGISTRY/$PACKAGE:latest --build-arg VERSION="${IMAGE_SOURCE_DISTRO}-${IMAGE_VERSION}"
+RETCODE=$?
+
+if [ "${RETCODE}" -ne 0 ] ; then
+    echo "\n\nBuild failed with return code $RETCODE"
+    exit $RETCODE
+fi
+
+echo ""
+echo "Build completeled for $REGISTRY/$PACKAGE:$VERSION using $SOURCE_DISTRO:$SOURCE_TAG"
