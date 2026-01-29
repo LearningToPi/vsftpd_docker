1a16b76ea551d3e109fab5ad617914a18a5fb270
Thomas Dunteman <104242297+LearningToPi@users.noreply.github.com>
2026-01-28 18:44:35 -0700

    updated build script to handle arm64 and riscv64

--- build.sh
+++ build.sh
@@ -1,11 +1,20 @@
 #!/bin/bash -v
 
-
+UNAME_ARCH=`uname -m`
 VERSION=$1
 SOURCE_DISTRO='ubuntu'
 SOURCE_TAG='latest'
 REGISTRY="docker.io"
 PACKAGE="learningtopi/vsftpd"
+
+ARCH=$(
+    case "$UNAME_ARCH" in
+	("x86_64") echo "amd64" ;;
+	("amd64") echo "amd64" ;;
+	("aarch64") echo "arm64" ;;
+	("arm64") echo "arm64" ;;
+	("riscv64") echo "riscv64" ;;
+    esac)
 
 docker pull docker.io/${SOURCE_DISTRO}:${SOURCE_TAG}
 RETCODE=$?
@@ -20,7 +29,7 @@ IMAGE_VERSION=`docker image inspect ${SOURCE_DISTRO}:${SOURCE_TAG} -f '{{index .
 echo ""
 echo "Pulled ${IMAGE_DISTRO}:${IMAGE_VERSION}"
 
-docker build -f Containerfile . -t $REGISTRY/$PACKAGE:${VERSION} -t $REGISTRY/$PACKAGE:latest --build-arg SOURCE_DISTRO="${IMAGE_DISTRO}" --build-arg SOURCE_TAG="${IMAGE_VERSION}" --build-arg BUILD_VERSION="${VERSION}" 
+docker build -f Containerfile . -t $REGISTRY/$PACKAGE:${VERSION}-$ARCH -t $REGISTRY/$PACKAGE:latest-$ARCH --build-arg SOURCE_DISTRO="${IMAGE_DISTRO}" --build-arg SOURCE_TAG="${IMAGE_VERSION}" --build-arg BUILD_VERSION="${VERSION}" 
 RETCODE=$?
 
 if [ "${RETCODE}" -ne 0 ] ; then
